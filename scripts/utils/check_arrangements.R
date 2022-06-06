42307feab57ca99f0f0d651ec648fddce2306ada
Levan Bokeria <levan.bokeria@gmail.com>
2022-06-06 19:22:09 +0100

    choosing arrangements

---
+++ scripts/utils/check_arrangements.R
@@ -0,0 +1,13 @@
+# Description ###############
+
+# This script checks that the arrangements we used for the 5 boards are "equal",
+# i.e. have the same number of PAs at the same border distances.
+
+# Setup #####################
+
+rm(list=ls())
+
+source('./scripts/utils/load_all_libraries.R')
+
+# Define the arrangements ###############
+
